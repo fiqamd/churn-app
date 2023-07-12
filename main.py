c0bae0d09a1346e7ba3765a6657195207cb22eb6
fiqamd <taufiqahmadi1122@gmail.com>
2023-07-12 13:58:07 +0700

    Percobaan keempat

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def load_model(bucket_name, model_path):
     return model
 
 # # Anda dapat menggunakan fungsi ini untuk meload model Anda
-model = load_model("model_churn", "https://storage.cloud.google.com/model_churn/model.pkl")
+model = load_model("model_churn", "gs://model_churn/model.pkll")
 # @st.cache_resource
 # def load_model()
 # conn = st.experimental_connection('gcs', type=FilesConnection)
