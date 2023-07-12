5860c1ae1cd3f24a72ec81a88f9d2d2e5e569cea
fiqamd <taufiqahmadi1122@gmail.com>
2023-07-12 13:14:23 +0700

    Percobaan menggunakan cache ttl

--- main.py
+++ main.py
@@ -27,30 +27,31 @@ import warnings
 warnings.filterwarnings('ignore')
 sys.modules['sklearn.externals.six'] = six
 
-@st.cache(allow_output_mutation=True)
-def load_model(bucket_name, model_path):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(model_path)
+# @st.cache(allow_output_mutation=True)
+# def load_model(bucket_name, model_path):
+#     storage_client = storage.Client()
+#     bucket = storage_client.bucket(bucket_name)
+#     blob = bucket.blob(model_path)
 
-    # Download as a string, then convert the string to BytesIO object
-    model_string = blob.download_as_text()
-    model_bytestream = io.BytesIO(model_string.encode())
+#     # Download as a string, then convert the string to BytesIO object
+#     model_string = blob.download_as_text()
+#     model_bytestream = io.BytesIO(model_string.encode())
 
-    model = joblib.load(model_bytestream)
+#     model = joblib.load(model_bytestream)
 
-    return model
+#     return model
 
-# Anda dapat menggunakan fungsi ini untuk meload model Anda
-model = load_model("model_churn", "model_churn/model.pkl")
-
-# conn = st.experimental_connection('gcs', type=FilesConnection)
-# model_path = conn.read("model_churn/model.pkl", input_format="pkl")
+# # Anda dapat menggunakan fungsi ini untuk meload model Anda
+# model = load_model("model_churn", "model_churn/model.pkl")
+# @st.cache_resource
+# def load_model()
+conn = st.experimental_connection('gcs', type=FilesConnection)
+model_path = conn.read("model_churn/model.pkl", input_format="pkl", ttl=3600)
 # # Ganti dengan path file model yang telah Anda simpan
 # # model_path = 'model/randfor_new3resampled_mode1.pkl'
 
 # # Muat model
-# model = joblib.load(model_path)
+model = joblib.load(model_path)
 
 area_dict = {'Tangerang': 0, 'Depok': 1, 'Cibubur': 2, 'Bekasi': 3, 'Surabaya': 4, 'Semarang': 5, 'Bogor': 6, 'Malang': 7, 'Palembang': 8, 'Jakarta': 9, 'Medan': 10, 'Bandung': 11, 'Bali': 12, 'Makassar': 13, 'Serang': 14, 'Pekanbaru': 15, 'Lampung': 16, 'Solo': 17, 'Cilegon': 18, 'Karawang': 19, 'Jambi': 20}
 
