9f00d18f05462afadb6f88d91c24e896b433430f
fiqamd <taufiqahmadi1122@gmail.com>
2023-07-20 04:15:05 +0700

    perbaikan testing nested pie area name

--- main.py
+++ main.py
@@ -287,16 +287,16 @@ def visualize_data_batch(data):
 
     # Membuat nested pie chart
     fig, ax = plt.subplots(figsize=(8, 8))
-    ax.pie(outer_sizes, labels=outer_labels, autopct='%1.1f%%', startangle=90, colors=outer_colors)
+    outer_pie, _ = ax.pie(outer_sizes, labels=outer_labels, autopct='%1.1f%%', startangle=90, colors=outer_colors)
     circle = plt.Circle((0, 0), 0.6, color='white')
     ax.add_artist(circle)
-    ax.pie(inner_sizes, labels=inner_labels, labeldistance=0.4, autopct='%1.1f%%', startangle=90, colors=inner_colors)
+    inner_pie, _ = ax.pie(inner_sizes, radius=0.6, labels=inner_labels, labeldistance=0.4, autopct='%1.1f%%', startangle=90, colors=inner_colors)
+
+    # Menambahkan legend untuk inner pie chart
+    plt.legend(inner_pie, inner_labels, title='Area Name', loc='upper right', bbox_to_anchor=(1.3, 1))
 
     # Menampilkan judul
     ax.set_title('Nested Pie Chart: Churn vs Area Name (Top 10)')
-
-    # Menampilkan legend
-    plt.legend(title='Area Name', loc='upper right', bbox_to_anchor=(1.3, 1))
 
     # Menampilkan nested pie chart menggunakan Streamlit
     st.pyplot(fig)
