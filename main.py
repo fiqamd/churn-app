9f78a5494ec62373836b9163ef942d9173bf3457
fiqamd <taufiqahmadi1122@gmail.com>
2023-07-23 00:07:13 +0700

    Fix 1.2-Menambahkan Komparasi Churn dan Non Churn

--- main.py
+++ main.py
@@ -656,7 +656,7 @@ def visualize_data_batch(data):
         # print(f'Proportions of not churn and churn in area code {area_names} = {churn0_proportion}, {churn1_proportion} respectively')
 
         # Visualisasi menggunakan Seaborn
-        churn_area = sns.catplot(x="churn", kind="count", hue="area_names", palette="magma", data=data, height=8)
+        churn_area = sns.catplot(x="Churn", kind="count", hue="area_names", palette="magma", data=data, height=8)
         for ax in churn_area.axes.flat:
             for c in ax.containers:
                 ax.bar_label(c)
