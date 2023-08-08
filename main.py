9654b48fc31548191d7d8073d65ebe05eba0cd43
fiqamd <taufiqahmadi1122@gmail.com>
2023-08-09 02:16:59 +0700

    Testing pemisahan pyplot full_adv ,five_adv , ten_adv-7.1.6

--- main.py
+++ main.py
@@ -771,8 +771,8 @@ def full_tvplan(data):
     plt.xticks(rotation=90)
 
     # Move the legend to below the bar chart
-    plot._legend.set_bbox_to_anchor((0.5, -0.15))
-    plot._legend.set_title('')
+    # plot._legend.set_bbox_to_anchor((0.5, -0.15))
+    # plot._legend.set_title('')
 
     fig = plt.gcf()
     return fig
